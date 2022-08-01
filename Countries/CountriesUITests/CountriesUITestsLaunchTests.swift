f3a7f178f9bb7404fe87873a40a26122e1300414
MohammadHossan <mohammadrasel83@outlook.com>
2022-08-01 23:15:50 +0100

    code refactor

--- Countries/CountriesUITests/CountriesUITestsLaunchTests.swift
+++ Countries/CountriesUITests/CountriesUITestsLaunchTests.swift
@@ -16,23 +16,4 @@ class CountriesUITestsLaunchTests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
-    func test_snapshot(){
-        
-        let app = XCUIApplication()
-        app.activate()
-        
-        let tablesQuery = XCUIApplication().tables
-        tablesQuery.staticTexts["name of country is Åland Islands and population is 28875"].otherElements.containing(.staticText, identifier:"Åland Islands").element.tap()
-        
-        XCTAssertTrue(tablesQuery.element.exists)
-        
-        tablesQuery.staticTexts["name of country is Albania and population is 2837743"].otherElements.containing(.staticText, identifier:"Albania").element.tap()
-        
-        XCTAssertTrue(tablesQuery.element.exists)
-        
-        XCUIApplication().tables.staticTexts["name of country is Andorra and population is 77265"].otherElements.containing(.staticText, identifier:"Andorra").element.tap()
-                
-        XCTAssertTrue(tablesQuery.element.exists)
-    }
-
 }

928af2b1acc8635e889736a619dc66d13e2bada1
MohammadHossan <mohammadrasel83@outlook.com>
2022-08-01 23:52:22 +0100

    snapshot testing

---
+++ Countries/CountriesTests/CountryViewControllerTest.swift
@@ -0,0 +1,28 @@
+//
+//  CountryViewControllerTest.swift
+//  CountriesTests
+//
+//  Created by M A Hossan on 01/08/2022.
+//
+
+import XCTest
+@testable import Countries
+import SnapshotTesting
+
+class CountryViewControllerTest: XCTestCase {
+    
+    var sut: CountriesViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "CountriesViewController") as! CountriesViewController
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func testInitialScreen(){
+        assertSnapshot(matching: sut, as: .image())
+    }
+}
